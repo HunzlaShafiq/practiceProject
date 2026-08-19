551357bb8a1c22f02da7d1a6ef79d5950b315863
Hunzla Shafiq <157482220+HunzlaShafiq@users.noreply.github.com>
2026-08-19 12:43:03 +0500

    update Onboarding screen

--- lib/Views/Shop_App/onboarding_view.dart
+++ lib/Views/Shop_App/onboarding_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:practice_project/Views/Shop_App/shop_view.dart';
 
 class OnboardingView extends StatefulWidget {
   const OnboardingView({super.key});
@@ -8,6 +9,9 @@ class OnboardingView extends StatefulWidget {
 }
 
 class _OnboardingViewState extends State<OnboardingView> {
+
+  bool _isPressed =false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,13 +24,88 @@ class _OnboardingViewState extends State<OnboardingView> {
               width: 190,
               height: 190,
               decoration: BoxDecoration(
-                color: Color(0xff56af9b).withOpacity(.1),
+                color: Color(0xff56af9b).withOpacity(0),
                 shape: BoxShape.circle,
                 boxShadow: [
                   BoxShadow(
-                    color: Color(0xff56af9b).withOpacity(.1),
-                    offset: const Offset(0, 5),
-                    blurRadius: 8,
+                    color: Color(0xff56af9b).withOpacity(.2),
+                    offset: const Offset(0, 0),
+                    blurRadius: 10,
+                    spreadRadius: 10,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            right: -150, // Half outside the screen
+            top: MediaQuery.of(context).size.height / 2 - 300,
+            child: Container(
+              width: 210,
+              height: 210,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.grey.shade400,
+                  width: 1,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            right: -100, // Half outside the screen
+            top: MediaQuery.of(context).size.height / 2 - 150,
+            child: Container(
+              width: 190,
+              height: 190,
+              decoration: BoxDecoration(
+                color: Color(0xff56af9b).withOpacity(0),
+                shape: BoxShape.circle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xff56af9b).withOpacity(.15),
+                    offset: const Offset(0, 0),
+                    blurRadius: 10,
+                    spreadRadius: 10,
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            left: -150, // Half outside the screen
+            top: MediaQuery.of(context).size.height / 2 - 30,
+            child: Container(
+              width: 190,
+              height: 190,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                border: Border.all(
+                  color: Colors.grey.shade400,
+                  width: 1,
+                ),
+              ),
+            ),
+          ),
+          Positioned(
+            left: -80,
+            top: MediaQuery.of(context).size.height / 2 - 30,
+            child: Container(
+              width: 190,
+              height: 190,
+
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(100),
+                    bottomRight: Radius.circular(30),
+                    bottomLeft: Radius.circular(100)),
+                color: Colors.grey.withOpacity(0),
+                shape: BoxShape.rectangle,
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(.15),
+                    offset: const Offset(0, 0),
+                    blurRadius: 10,
                     spreadRadius: 10,
                   ),
                 ],
@@ -74,57 +153,42 @@ class _OnboardingViewState extends State<OnboardingView> {
                     fontSize: 19),
               ),
               const SizedBox(height: 60,),
-              Container(
-                height: 70,
-                width: 70,
+              GestureDetector(
+                onTapDown: (_) => setState(() => _isPressed = true),
+                onTapUp: (_) => setState(() => _isPressed = false),
+                onTapCancel: () => setState(() => _isPressed = false),
+                onTap: (){
+                      Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(builder: (context)=>ShopView()));
+                },
+                child: AnimatedScale(
+                  scale: _isPressed ? .95:1.0,
+                  duration:const Duration(milliseconds: 100),
+                  child: Container(
+                    height: 70,
+                    width: 70,
 
-                decoration: BoxDecoration(
-                    boxShadow: [
-                      BoxShadow(
-                        color: const Color(0xff56af9b),
-                        offset: const Offset(0, 5),
-                        blurRadius: 8,
-                        spreadRadius: 0,
-                      ),
-                    ],
-                  color: Color(0xff56af9b),
-                  borderRadius: BorderRadius.circular(100)
+                    decoration: BoxDecoration(
+                        boxShadow: [
+                          BoxShadow(
+                            color: const Color(0xff56af9b),
+                            offset: const Offset(0, 5),
+                            blurRadius: 8,
+                            spreadRadius: 0,
+                          ),
+                        ],
+                      color: Color(0xff56af9b),
+                      borderRadius: BorderRadius.circular(100)
+                    ),
+                    child: Center(child: Text("GO",style: TextStyle(
+                        color: Colors.white,fontSize: 22),)),
+                  ),
                 ),
-                child: Center(child: Text("GO",style: TextStyle(
-                    color: Colors.white,fontSize: 22),)),
               )
             ],
           ),
-          Positioned(
-            right: -150, // Half outside the screen
-            top: MediaQuery.of(context).size.height / 2 - 300,
-            child: Container(
-              width: 210,
-              height: 210,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(
-                  color: Colors.grey.shade400,
-                  width: 1,
-                ),
-              ),
-            ),
-          ),
-          Positioned(
-            left: -150, // Half outside the screen
-            top: MediaQuery.of(context).size.height / 2 - 30,
-            child: Container(
-              width: 190,
-              height: 190,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(
-                  color: Colors.grey.shade400,
-                  width: 1,
-                ),
-              ),
-            ),
-          ),
+
 
 
 
